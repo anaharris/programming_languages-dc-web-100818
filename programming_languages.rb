4c94162668933a0fa14875e8d0fbe49bf032cd39
Ana Harris <vana1990@gmail.com>
2018-10-03 01:36:38 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, types|
     lang.each do |name, types|
-      new_hash[name] = 
+      new_hash[name] = types
     end
   end
 end
